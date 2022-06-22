26baee15f3782a02fe97f6cbc7e9f661fe85a343
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-06-22 10:01:27 +0700

    Demonstrating add new node conventionally

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -9,10 +9,11 @@ struct Node {
 };
 
 int main() {
-    Node *node1, *node2;
+    Node *node1, *node2, *node3;
 
     node1 = new Node();
     node2 = new Node();
+    node3 = new Node();
 
     node1->data = 44;
     node1->prev = NULL;
@@ -20,7 +21,11 @@ int main() {
 
     node2->data = 55;
     node2->prev = node1;
-    node2->next = NULL;
+    node2->next = node3;
+
+    node3->data = 88;
+    node3->prev = node2;
+    node3->next = NULL;
 
     Node *cur;
     cur = node1;
