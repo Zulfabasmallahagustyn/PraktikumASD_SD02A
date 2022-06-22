a1d0878914b64e97f26e6864dfa75a3207a5c49f
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2022-06-22 09:52:01 +0700

    Buat variabel pointer untuk node1 dan node2

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -8,4 +8,17 @@ struct Node {
     Node *next;
 };
 
+int main() {
+    Node *node1, *node2;
 
+    node1 = new Node();
+    node2 = new Node();
+
+    node1->data = 44;
+    node1->prev = NULL;
+    node1->next = node2;
+
+    node2->data = 55;
+    node2->prev = node1;
+    node2->next = NULL;
+}
